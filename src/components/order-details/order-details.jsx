18a98ac44f4451450e7f7873d8fd425507648b37
im-Arn <110503940+im-Arn@users.noreply.github.com>
2023-04-08 06:28:52 +0300

    React.Sprint2 Rev.1 add props,proptypes

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,9 +1,10 @@
 import Style from './order-details.module.css'
+import PropTypes from 'prop-types';
 
-export default function OrderDetails() {
+export default function OrderDetails( {number}) {
   return ( 
     <div className={Style.content}>
-      <p className={`text text_type_digits-large ${Style.number}`}>034536</p>
+      <p className={`text text_type_digits-large ${Style.number}`}>{number}</p>
       <p className="text text_type_main-medium mt-8 mb-15">идентификатор заказа</p>
       <div className={Style.image}></div>
       <p className="text text_type_main-default mt-15 mb-2">Ваш заказ начали готовить</p>
@@ -11,3 +12,8 @@ export default function OrderDetails() {
     </div>
   );
 }
+
+OrderDetails.propTypes = {
+    number: PropTypes.number.isRequired
+  }
+
