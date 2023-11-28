47688ca07af1fd123b9d6167d96bf2f6ed33b71e
im-Arn <110503940+im-Arn@users.noreply.github.com>
2023-11-28 14:39:25 +0300

    test gh-p fix

--- src/pages/not-found-page/not-found-page.jsx
+++ src/pages/not-found-page/not-found-page.jsx
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
   return (
     <article className={Style.nf}>
       <p className='text text_type_digits-large text_color_active'>4O4</p>
-      <p className='text text_type_main-large text_color_active'>Айяйяй</p>
+      <p className='text text_type_main-large text_color_active'>Айяй, а где роутинг </p>
     </article>
   )
 };
